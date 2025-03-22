038cbf20caea0409fb77fa0ee85f16283038afef
Protazer <florian.huberdeau@laposte.net>
2025-03-22 16:32:51 +0100

    feat(prooject): Spring-boot-project initialization

---
+++ src/test/java/com/chatop/chaTop/ChaTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatop.chaTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
